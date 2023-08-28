ce8c90624a87b085165013bd48c2d375584c5d7b
Martin Skalický <m.skalicky@nextap.eu>
2023-08-28 19:07:41 +0200

    Remove dependency on config from library

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,7 +1,6 @@
-import { config } from "../config.ts";
 // @deno-types="npm:@types/node-localstorage"
 import { LocalStorage } from "npm:node-localstorage";
 
 export const localStorage = new LocalStorage(
-  config.STORAGE_PATH
+  Deno.env.get("STORAGE_PATH") || "local-storage"
 );
